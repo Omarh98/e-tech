62518a565fe47926444d4d35c2d07820c045f2d2
Omar Hossam <omar_hossam98@yahoo.com>
2021-06-07 12:54:09 +0200

    hi

--- public/main5.js
+++ public/main5.js
@@ -5,12 +5,12 @@ var parsedResponse2="";
 loadbillingcards();
 function loadbillingcards(){
     checkoutRequest.open('GET',checkoutURL,true);
-    checkoutRequest.addEventListener('load',onLoad);
+    checkoutRequest.addEventListener('load',onLoad2);
    // checkoutRequest.addEventListener('error',onError);
     checkoutRequest.send();
 }
 
-function onLoad(){
+function onLoad2(){
     var response =this.responseText;
     parsedResponse2=JSON.parse(response);
     console.log(parsedResponse2);
